594eab107d7ac482e375f3911c4757dfcf4231d1
JuanEspares <juloespares@yahoo.com.au>
2020-09-20 21:00:40 +1000

    StrongHeap working; need to to algorithmic analysis.

--- src/StrongHeap.java
+++ src/StrongHeap.java
@@ -1,7 +1,8 @@
 import java.util.*;
-
 public class StrongHeap {
     /**
+     * Time Complexity:
+     * Space Complexity:
      * Determines whether the binary tree with the given root node is a "strong
      * binary heap", as described in the assignment task sheet.
      * <p>
@@ -17,8 +18,8 @@ public class StrongHeap {
         boolean nullVal = false;
         queue.add(root);
 
+        //Traverse tree level ordered.
         while (!queue.isEmpty()){
-
             BinaryTree<Integer> current = queue.poll();
 
             if (current == null){
@@ -30,35 +31,46 @@ public class StrongHeap {
                 }
                 queue.add(current.getLeft());
                 queue.add(current.getRight());
-                compareNodes(current);
+                if (!compareNodes(current)){
+                    return false;
+                }
             }
         }
         return true;
     }
 
+    /**
+     * Time Complexity:
+     * Space Complexity:
+     *
+     * @param current
+     * @return
+     */
     private static boolean compareNodes (BinaryTree<Integer> current){
-
         BinaryTree<Integer> leftChild = current.getLeft();
         BinaryTree<Integer> rightChild = current.getRight();
-
+        //Check if left node + Parent < Grandparent
         if (leftChild != null){
             BinaryTree<Integer> leftChildChild = leftChild.getLeft();
             if (leftChildChild != null && leftChildChild.getValue()
                     + leftChild.getValue() >= current.getValue()){
                 return false;
             }
-            if (leftChild.getValue() > current.getValue()) {
+            //check if left node is less than parent.
+            if (leftChild.getValue() >= current.getValue()) {
                 return false;
             }
         }
-
+        //Check if right node + Parent < Grandparent
         if (rightChild != null){
             BinaryTree<Integer> rightChildChild = rightChild.getRight();
             if (rightChildChild != null && rightChildChild.getRight().getValue()
                     + rightChild.getValue() >= current.getValue()){
                 return false;
             }
-            if (rightChild.getValue() > current.getValue()){
+
+            //Check if right node is less than parent.
+            if (rightChild.getValue() >= current.getValue()){
                 return false;
             }
         }
