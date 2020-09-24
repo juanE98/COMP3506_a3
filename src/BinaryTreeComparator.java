488fe60b3437c76a8a92507d6b8a9d4c4527f23d
JuanEspares <juloespares@yahoo.com.au>
2020-09-24 19:50:37 +1000

    BinaryTreeComparator Test 3/4

--- src/BinaryTreeComparator.java
+++ src/BinaryTreeComparator.java
@@ -17,25 +17,43 @@ public class BinaryTreeComparator<E extends Comparable<E>> implements Comparator
      */
     @Override
     public int compare(BinaryTree<E> tree1, BinaryTree<E> tree2) {
-        if (tree1 == null){
+
+        //Base Case: Leaf node reached.
+        if (tree1 == null && tree2 == null) {
+            return 0;
+        }
+        else if (tree1 == null && tree2 != null) {
             return -1;
         }
-
-        compare(tree1.getLeft(),tree2.getLeft());
-        if (tree1.getValue().compareTo(tree2.getValue()) > 0){
+        else if (tree1 != null && tree2 == null) {
             return 1;
         }
-        else if (tree1.getValue().compareTo(tree2.getValue()) < 0){
-            return -1;
+        else{
+            //Recursively compare nodes starting from left most leaf.
+            if (comparison(tree1,tree2) == 0) {
+                if (compare(tree1.getLeft(),tree2.getLeft()) == 0) {
+                    if (compare(tree1.getRight(),tree2.getRight()) == 0) {
+                        return 0;
+                    }
+                    else {
+                        return comparison(tree1.getRight(),tree2.getRight());
+                    }
+                }
+                else {
+                    return comparison(tree1.getLeft(),tree2.getLeft());
+                }
+            }
         }
 
-        if (comparison(tree1.getRight(),tree2.getRight()) != 0) {
-            return comparison(tree1.getRight(),tree2.getRight());
-        }
-
-        return 0;
+        return comparison(tree1,tree2);
     }
 
+    /**
+     * Helper method to compare the values between node1 and node2.
+     * @param tree1 root of the first binary tree, may be null.
+     * @param tree2 root of the second binary tree, may be null.
+     * @return
+     */
     private int comparison(BinaryTree<E> tree1, BinaryTree<E> tree2){
         if (tree1 == null && tree2 != null){
             return -1;
@@ -44,11 +62,11 @@ public class BinaryTreeComparator<E extends Comparable<E>> implements Comparator
             return 1;
         }
         if (tree1 != null && tree2 != null) {
-            if (tree1.getValue().compareTo(tree2.getValue()) > 0) {
-                return 1;
-            }
-            else if (tree1.getValue().compareTo(tree2.getValue()) < 0) {
+            if (tree1.getValue().compareTo(tree2.getValue()) < 0) {
                 return -1;
+            }
+            else if (tree1.getValue().compareTo(tree2.getValue()) > 0) {
+                return 1;
             }
         }
         return 0;
